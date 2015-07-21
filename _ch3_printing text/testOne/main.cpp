ad690674fa0e7387f25a384dcc52206d96d2fce0
gear20056 <gear20056@hotmail.com>
2015-07-21 09:35:24 +0700

    ch3_v1_added

---
+++ _ch3_printing text/testOne/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    cout << "boy i love bacon ";
+    cout << "and ham";
+    */
+
+    /*
+    cout << "boy i love bacon " << endl;
+    cout << "and ham";
+    */
+
+    /*
+    cout << "boy i love bacon \n";
+    cout << "and ham";
+    */
+
+    /*
+    cout << "boy i love bacon \n\n";
+    cout << "and ham";
+    */
+
+    cout << "boy \n i \n love \n bacon \n";
+
+    return 0;
+}
