276bc4d879873c086ec97698e288516238969899
gear20056 <gear20056@hotmail.com>
2015-07-21 11:07:44 +0700

    ch6_v1_added

---
+++ _ch6_variables memory concepts/testOne/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    int tuna;
+    tuna = 54;
+
+    */
+
+    /*
+    int tuna;
+    int tuna = 32; //DO NOT repeat type again!!
+    */
+
+    int tuna = 99;
+    tuna = 76; //replace tuna to '76'
+    cout << tuna;
+
+    return 0;
+}
