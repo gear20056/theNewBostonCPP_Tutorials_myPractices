47138743f89ac99dfd8725fe8d7790b8d2e0fe7e
gear20056 <gear20056@hotmail.com>
2015-07-21 09:27:46 +0700

    ch4_v1_added

---
+++ _ch4_variables/testOne/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    int tuna = 6;
+    cout << tuna;
+    */
+
+    /*
+    int a = 4;
+    int b = 21;
+
+    int sum = a + b;
+
+    cout << sum;
+    */
+
+    /*
+    int a = 4;
+    int b = 21;
+
+    int sum = b-a;
+
+    cout << sum;
+    */
+
+
+    int a = 4;
+    int b = 21;
+
+    int sum = 48 - 7;
+
+    cout << sum;
+
+
+
+    return 0;
+}
