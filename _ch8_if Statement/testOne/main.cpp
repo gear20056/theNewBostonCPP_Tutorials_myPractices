ff1c79ef50620bc1a6ff9f7d0cfab9f47af8f403
gear20056 <gear20056@hotmail.com>
2015-07-24 15:54:09 +0700

    ch8_v1_added

---
+++ _ch8_if Statement/testOne/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    if(1<3){
+        cout << "Hayama is AWESOME!!!!";
+    }
+    */
+
+    /*
+    if(3<2){
+        cout << "Hayama is AWESOME!";
+    }
+    */
+
+    /*
+    if(3==3){
+            cout << "Hayama is AWESOME!!!!";
+
+    }
+    */
+
+    /*
+    int a = 98;
+    int b = 76;
+
+    if(a > b){
+                    cout << "Hayama is AWESOME!!!!";
+
+    }
+    */
+
+    return 0;
+}
